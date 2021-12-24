2fc64cd57e40818af49ac8022cd847c96e91a5af
Andre Korkin <andre_korkin@mail.ru>
2021-12-24 11:02:10 +0600

    add navBar

--- src/app.jsx
+++ src/app.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { Redirect, Route, Switch } from 'react-router-dom'
+import NavBar from './components/navbar'
+import Home from './components/home'
 import Users from './components/users'
+import LogIn from './components/login'
+import Error404 from './components/error404'
 
 
 const App = () => {
-    return <Users />
+    return (
+        <>
+            <NavBar />
+            <Switch>
+                <Route path='/' exact component={Home} />
+                <Route path='/users' component={Users} />
+                <Route path='/login' component={LogIn} />
+                <Route path='/404' component={Error404} />
+
+                <Redirect to='/404' />
+            </Switch>
+        </>
+    )
 }
 
 export default App
---
+++ src/components/login.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+
+const LogIn = () => {
+    return <h1>LogIn</h1>
+}
+
+export default LogIn
