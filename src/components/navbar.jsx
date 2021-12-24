10c6bf17792e8e3a11e74365bfd49c08b6085241
Andre Korkin <andre_korkin@mail.ru>
2021-12-24 11:21:47 +0600

    NavBar complited

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,16 +6,26 @@ const NavBar = () => {
     return (
         <ul className='nav'>
             <li className='nav-item'>
-                <Link className='nav-link' to='/'>Home</Link>
+                <Link className='nav-link' to='/' onClick={event => handleClick(event)}>Home</Link>
             </li>
             <li className='nav-item'>
-                <Link className='nav-link' to='/users'>Users</Link>
+                <Link className='nav-link' to='/users' onClick={event => handleClick(event)}>Users</Link>
             </li>
             <li className='nav-item'>
-                <Link className='nav-link' to='/login'>Login</Link>
+                <Link className='nav-link' to='/login' onClick={event => handleClick(event)}>Login</Link>
             </li>
         </ul>
     )
+
+
+    function handleClick (event) {
+        document.querySelectorAll('.nav-link').forEach(link => {
+            link.style.background = '#fff'
+            link.style.color = '#0d6efd'
+        })
+        event.target.style.background = '#5c636a'
+        event.target.style.color = '#fff'
+    }
 }
 
 export default NavBar
