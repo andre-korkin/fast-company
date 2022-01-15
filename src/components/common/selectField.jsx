b2d7150492c373d9bad10a171fc5cf6896b8b706
Andre Korkin <andre_korkin@mail.ru>
2022-01-15 09:14:55 +0600

    lesson Form2 - 3.Select

---
+++ src/components/common/selectField.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+
+const SelectField = ({ label, name, data, errors, onChange }) => {
+    const getInpClass = () => errors === undefined || errors.length === 0 ? 'form-select' : 'form-select is-invalid'
+
+    return (
+        <div className='mb-5'>
+            <label htmlFor={name}>{label}</label>
+            <div className="input-group has-validation">
+                <select id={name} name={name} value='' errors={errors.profession} onChange={onChange} className={getInpClass()}>
+                    <option disabled value=''>Выбрать...</option>
+                    {data && data.map(item => <option key={item._id} value={item._id}>{item.name}</option>)}
+                </select>
+                {errors && <div className='invalid-feedback'>{errors[0]}</div>}
+            </div>
+        </div>
+    )
+}
+
+SelectField.propTypes = {
+    label: PropTypes.string,
+    name: PropTypes.string,
+    data: PropTypes.array,
+    errors: PropTypes.array,
+    onChange: PropTypes.func
+}
+
+export default SelectField
--- src/components/page/registerForm.jsx
+++ src/components/page/registerForm.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect } from 'react'
 import TextField from '../common/textField'
 import { validator } from '../../utils/validator'
+import API from '../../api'
+import SelectField from '../common/selectField'
 
 
 const RegisterForm = () => {
-    const [data, setData] = useState({ email: '', password: '' })
+    const [data, setData] = useState({ email: '', password: '', profession: '' })
     const [errors, setErrors] = useState({})
+
+    const [profs, setProfs] = useState()
+    useEffect(() => API.professions.fetchAll().then(data => setProfs(data)), [])
 
     const validatorConfig = {
         email: {
@@ -20,6 +25,9 @@ const RegisterForm = () => {
                 message: 'Длина пароля должна быть не менее 8 символов',
                 value: 8
             }
+        },
+        profession: {
+            isRequired: { message: 'Необходимо указать профессию' }
         }
     }
 
@@ -33,6 +41,17 @@ const RegisterForm = () => {
             <form onSubmit={handleSubmit}>
                 <TextField label='Email' name='email' value={data.email} errors={errors.email} onChange={handleChange} />
                 <TextField label='Пароль' type='password' name='password' value={data.password} errors={errors.password} onChange={handleChange} />
+                {profs && <SelectField label='Профессия' name='profession' value={data.profession} data={profs} errors={errors.profession} onChange={handleChange} />}
+                {/* <div className="mb-5">
+                    <label htmlFor="validationCustom04" className="form-label">Профессия</label>
+                    <select className="form-select" id="validationCustom04" name='profession' value={data.profession} errors={errors.profession} onChange={handleChange}>
+                        <option disabled value=''>Выбрать...</option>
+                        {profs && profs.map(prof => <option key={prof._id} value={prof._id}>{prof.name}</option>)}
+                    </select>
+                    <div className="invalid-feedback">
+                        Необходимо указать профессию.
+                    </div>
+                </div> */}
                 <button className='btn btn-primary w-100 mx-auto' disabled={Object.values(errors).join('').trim() !== ''}>Зарегистрироваться</button>
             </form>
         </>
