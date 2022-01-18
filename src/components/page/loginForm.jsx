c7139d24e25608b075518be88f596960ddf71cb0
Andre Korkin <andre_korkin@mail.ru>
2022-01-18 11:02:25 +0600

    lesson Form2 complited

--- src/components/page/loginForm.jsx
+++ src/components/page/loginForm.jsx
@@ -2,7 +2,6 @@ import React, { useState, useEffect } from 'react'
 import TextField from '../common/form/textField'
 import { validator } from '../../utils/validator'
 import CheckField from '../common/form/checkField'
-import * as yup from 'yup'
 
 
 const LoginForm = () => {
@@ -10,15 +9,6 @@ const LoginForm = () => {
     const [errors, setErrors] = useState({})
 
     const checkText = <span>Оставаться в системе</span>
-
-    const validateScheme = yup.object().shape({
-        email: yup.string().required('Email обязателен для заполнения').email('Неверный формат Email'),
-        password: yup.string().required('Пароль обязателен для заполнения')
-            .matches(/(?=.*[A-Z])/, 'Пароль должен содержать хотя бы одну заглавную букву')
-            .matches(/(?=.*[0-9])/, 'Пароль должен содержать хотя бы одну цифру')
-            .matches(/(?=.*[!@#$%^&_])/, 'Пароль должен содержать хотя бы один спецсимвол из этого списка: ! @ # $ % ^ & _')
-            .matches((/?=.*{8,}/), 'Длина пароля должна быть не менее 8 символов')
-    })
 
     const validatorConfig = {
         email: {
