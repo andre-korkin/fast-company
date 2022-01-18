26b0463714ffd276bf1b2ef72f01cc23fee82ed2
Andre Korkin <andre_korkin@mail.ru>
2022-01-18 20:35:30 +0600

    task Form2: edit userPage: add handleFavorite and handleDelete

--- src/components/page/userPage/user.jsx
+++ src/components/page/userPage/user.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useHistory } from 'react-router-dom'
 import API from '../../../api'
 import Qualities from '../../ui/qualities'
+import Favorite from '../../common/favorite'
 import PropTypes from 'prop-types'
 
 
@@ -13,20 +14,43 @@ const User = ({ id }) => {
 
     const userData = user && (
         <>
-            <h1>{user.name}</h1><br />
+            <h1>{user.name} <Favorite user={user} onFavorite={handleFavorite} /></h1>
+            <br />
+            <h4>{`Email: ${user.email}`}</h4>
+            <h4>{`Пол: ${user.sex}`}</h4>
             <h4>{`Профессия: ${user.profession.name}`}</h4>
             <div style={{ margin: '20px 0' }}><Qualities qualities={user.qualities} /></div>
             <h4>{`Завершенных свиданий: ${user.completedMeetings}`}</h4>
             <h4>{`Рейтинг: ${user.rate}`}</h4>
             <br />
-            <button className='btn btn-primary m-1' onClick={handleSave}>Весь список</button>
+            <button className='btn btn-warning m-1' onClick={handleEdit}>Редактировать</button>
+            <button className='btn btn-danger m-1' onClick={handleDelete}>Удалить</button>
         </>
     )
 
     return user ? userData : 'Loading data...'
 
 
-    function handleSave () {
+    function handleFavorite (id) {
+        const newArray = JSON.parse(localStorage.getItem('users')).map(user => {
+            if (user._id === id) {
+                const currentUser = { ...user, favorite: !user.favorite }
+                setUser(currentUser)
+                return currentUser
+            }
+            return user
+        })
+        localStorage.setItem('users', JSON.stringify(newArray))
+    }
+
+    function handleEdit () {
+        history.push('/users')
+    }
+
+    function handleDelete () {
+        const id = user._id
+        const newArray = JSON.parse(localStorage.getItem('users')).filter(user => user._id !== id)
+        localStorage.setItem('users', JSON.stringify(newArray))
         history.push('/users')
     }
 }
