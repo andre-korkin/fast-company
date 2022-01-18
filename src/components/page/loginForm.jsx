6f72ecb8a829555f7fd7cb2b69f5ba749b0bcfea
Andre Korkin <andre_korkin@mail.ru>
2022-01-18 08:24:20 +0600

    edit loginForm and init yup

--- src/components/page/loginForm.jsx
+++ src/components/page/loginForm.jsx
@@ -1,11 +1,24 @@
 import React, { useState, useEffect } from 'react'
 import TextField from '../common/form/textField'
 import { validator } from '../../utils/validator'
+import CheckField from '../common/form/checkField'
+import * as yup from 'yup'
 
 
 const LoginForm = () => {
-    const [data, setData] = useState({ email: '', password: '' })
+    const [data, setData] = useState({ email: '', password: '', checkText: true })
     const [errors, setErrors] = useState({})
+
+    const checkText = <span>Оставаться в системе</span>
+
+    const validateScheme = yup.object().shape({
+        email: yup.string().required('Email обязателен для заполнения').email('Неверный формат Email'),
+        password: yup.string().required('Пароль обязателен для заполнения')
+            .matches(/(?=.*[A-Z])/, 'Пароль должен содержать хотя бы одну заглавную букву')
+            .matches(/(?=.*[0-9])/, 'Пароль должен содержать хотя бы одну цифру')
+            .matches(/(?=.*[!@#$%^&_])/, 'Пароль должен содержать хотя бы один спецсимвол из этого списка: ! @ # $ % ^ & _')
+            .matches((/?=.*{8,}/), 'Длина пароля должна быть не менее 8 символов')
+    })
 
     const validatorConfig = {
         email: {
@@ -33,6 +46,7 @@ const LoginForm = () => {
             <form onSubmit={handleSubmit}>
                 <TextField label='Email' name='email' value={data.email} errors={errors.email} onChange={handleChange} />
                 <TextField label='Пароль' type='password' name='password' value={data.password} errors={errors.password} onChange={handleChange} />
+                <CheckField label={checkText} name='checkText' isCheck={data.checkText} onChange={handleChange} />
                 <button className='btn btn-primary w-100 mx-auto' disabled={Object.values(errors).join('').trim() !== ''}>Войти</button>
             </form>
         </>
@@ -40,7 +54,12 @@ const LoginForm = () => {
 
 
     function handleChange ({ target }) {
-        setData((prevState) => ({ ...prevState, [target.name]: target.value }))
+        if (target.type === 'checkbox') {
+            setData((prevState) => ({ ...prevState, [target.name]: !data[target.name] }))
+        }
+        else {
+            setData((prevState) => ({ ...prevState, [target.name]: target.value }))
+        }
     }
 
     function handleSubmit (event) {
